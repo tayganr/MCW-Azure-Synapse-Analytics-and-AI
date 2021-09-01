bd61fb35a7a7f0b09bd8c97e1b79c8996edc77c3
Taygan Rifat <taygan@gmail.com>
2021-09-01 15:56:33 +0100

    update

---
+++ templates/ps1/postDeploymentScript2.ps1
@@ -0,0 +1,23 @@
+param(
+    [string]$resourceGroupName,
+    [string]$storageAccountName,
+    [string]$synapseWorkspaceName
+)
+
+Install-Module Az.Synapse -Force
+
+
+$storageAccountKey = (Get-AzStorageAccountKey -ResourceGroupName $resourceGroupName -AccountName $storageAccountName)[0].Value
+# Linked Services
+$linkedService1 = @{
+    name = "${storageAccountName}"
+    type = "Microsoft.Synapse/workspaces/linkedservices"
+    properties = @{
+        type = "AzureBlobStorage"
+        typeProperties = @{
+            connectionString = "DefaultEndpointsProtocol=https;AccountName=${storageAccountName};AccountKey=${storageAccountKey};EndpointSuffix=core.windows.net;"
+        }
+    }
+}
+ConvertTo-Json $linkedService1  | Out-File ls1.json
+Set-AzSynapseLinkedService -WorkspaceName $synapseWorkspaceName -Name $storageAccountName -DefinitionFile "ls1.json"
